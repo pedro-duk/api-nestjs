c337f1554187e8c998e6c88999ed887fbc55206e
Pedro Luiz <pedro.c@compasso.com.br>
2023-09-20 15:18:00 -0400

    Implementar CRUD para Class

--- src/class/class.controller.ts
+++ src/class/class.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ClassRepository } from './class.repository';
 import { CreateClassDTO } from './dto/CreateClass.dto';
+import { UpdateClassDTO } from './dto/UpdateClass.dto';
 
 @Controller('/classes')
 export class ClassController {
@@ -13,6 +22,49 @@ export class ClassController {
     return {
       class: classData,
       message: 'Class saved successfully',
+    };
+  }
+
+  @Get()
+  async getAllClasses() {
+    const classes = await this.classRepository.findAll();
+
+    return {
+      classes,
+      message: 'Classes recovered',
+    };
+  }
+
+  @Get('/:id')
+  async getClassById(@Param('id') id: string) {
+    const currentClass = await this.classRepository.findById(id);
+
+    return {
+      class: currentClass,
+      message: 'Class found',
+    };
+  }
+
+  @Patch('/:id')
+  async updateClass(
+    @Param('id') id: string,
+    @Body() classUpdateData: UpdateClassDTO,
+  ) {
+    const currentClass = await this.classRepository.update(id, classUpdateData);
+
+    return {
+      updatedClass: currentClass,
+      message: 'Class has been updated',
+    };
+  }
+
+  @Delete('/:id')
+  async deleteClass(@Param('id') id: string) {
+    const currentClass = await this.classRepository.delete(id);
+
+    return {
+      deletedClass: currentClass,
+      message: 'Class has been deleted',
     };
   }
 }
--- src/class/class.repository.ts
+++ src/class/class.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import Class from "./class.entity";
+import Class, { IClass } from "./class.entity";
 import { CreateClassDTO } from "./dto/CreateClass.dto";
 
 @Injectable()
@@ -7,5 +7,23 @@ export class ClassRepository {
   async save(classData: CreateClassDTO) {
     const newClass = new Class(classData);
     await newClass.save();
+  }
+
+  async findById(classId: string) {
+    return await Class.findById(classId);
+  }
+
+  async update(classId: string, classUpdateData: Partial<IClass>) {
+    return await Class.findOneAndUpdate({ _id: classId }, classUpdateData, {
+      new: true,
+    });
+  }
+
+  async delete(classId: string) {
+    return await Class.findByIdAndDelete(classId);
+  }
+
+  async findAll() {
+    return await Class.find({});
   }
 }
